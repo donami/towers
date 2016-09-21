cc7a6e1cdd5bae24b143638411ac2824a2fd35fe
Markus Hederström <d0namiz@gmail.com>
2016-09-21 21:16:55 +0200

    Added pagination for towers on first page

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -3,17 +3,31 @@ angular.module('towersApp')
 
     $scope.towers = [];
 
+    $scope.totalItems = 0;
+    $scope.currentPage = 1;
+    $scope.numPerPage = 5;
+
     initTowers();
 
     function initTowers() {
       TowerFactory.getTowers()
         .then(function(response) {
-          var data = response.data.slice(1,10);              // TODO: should not be sliced, obviously. Find a way to display towers. All towers in a list may be too much.
+          var data = response.data;
 
           $scope.towers = data;
+          $scope.totalItems = response.data.length;
         }, function(error) {
           $scope.status = 'Unable to load towers: ' + error.message;
         });
     }
+
+    // Filter for paginating the results
+    $scope.paginate = function(value) {
+      var begin, end, index;
+      begin = ($scope.currentPage - 1) * $scope.numPerPage;
+      end = begin + $scope.numPerPage;
+      index = $scope.towers.indexOf(value);
+      return (begin <= index && index < end);
+    };
 
   }]);
