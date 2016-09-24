e71079aec1b7fe1eaa157164dab062383f5b96ca
Markus Hederström <d0namiz@gmail.com>
2016-09-25 01:44:56 +0200

    Added graph of most claims per day on the me-page

--- app/controllers/me.controller.js
+++ app/controllers/me.controller.js
@@ -1,6 +1,6 @@
 angular.module('towersApp')
-  .controller('MeController', ['$scope', '$cookies', 'MeFactory', 'TowerFactory',
-  function($scope, $cookies, MeFactory, TowerFactory) {
+  .controller('MeController', ['$scope', '$cookies', 'MeFactory',
+  function($scope, $cookies, MeFactory) {
 
     $scope.userApiKey = $cookies.get('userApiKey');
 
@@ -9,6 +9,11 @@ angular.module('towersApp')
     $scope.totalItems = 0;
     $scope.currentPage = 1;
     $scope.numPerPage = 5;
+
+    // For the graph of most claims per day
+    $scope.claimDaysLabels = [];
+    $scope.claimDaysSeries = ['Days with most claims'];
+    $scope.claimDaysData = [];
 
     $scope.lastClaimedTower = {
       tower_id: 0,
@@ -20,6 +25,8 @@ angular.module('towersApp')
       .then(function(response) {
         $scope.claimedTowers = response.data;
         $scope.totalItems = response.data.length;
+
+        getDaysWithMostClaims(response.data);
 
       }, function(error) {
         console.log(error);
@@ -58,4 +65,27 @@ angular.module('towersApp')
       return (begin <= index && index < end);
     };
 
+
+    // Get the days that user has claimed the most towers
+    function getDaysWithMostClaims(data) {
+      var data = data.map(function(obj) {
+        obj.claimed_on = obj.claimed_on.substr(0, 10);
+        return obj;
+      });
+
+      var counts = _.countBy(data, 'claimed_on');
+      var sortable = _.pairs(counts);
+
+      sortable.sort(function(a, b) {
+        return b[1] - a[1];
+      });
+
+      sortable = sortable.slice(0, 10);
+      console.log(sortable);
+
+      sortable.forEach(function(obj) {
+        $scope.claimDaysLabels.push(obj[0]);
+        $scope.claimDaysData.push(obj[1]);
+      });
+    }
   }]);
