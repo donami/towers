892a5db58067686e441bfede8819b208562fc7a5
Markus Hederström <d0namiz@gmail.com>
2016-09-25 18:49:30 +0200

    Sorting of leaderboard table

--- app/controllers/leaderboard.controller.js
+++ app/controllers/leaderboard.controller.js
@@ -3,15 +3,40 @@ angular.module('towersApp')
 
     $scope.leaderboard = [];
 
+    $scope.orderBy = 'claim_count';
+    $scope.reverseOrder = false;
+
     getLeaderboard();
+
+    $scope.setSort = function(property, asFloat) {
+      sort(property, asFloat);
+    }
+
+    function sort(property, asFloat) {
+      if (property == $scope.orderBy) $scope.reverseOrder = !$scope.reverseOrder;
+
+      // Parse as float if needed
+      if (asFloat) {
+        $scope.leaderboard.map(function(obj) {
+          obj[property] = parseFloat(obj[property]);
+          return obj;
+        });
+      }
+
+      $scope.orderBy = property;
+    }
 
     function getLeaderboard() {
       TowerFactory.getLeaderboard()
         .then(function(response) {
           $scope.leaderboard = response.data;
-        }, function(error) {
+        })
+        .then(function() {
+          sort($scope.orderBy, true);
+        })
+        .catch(function(error) {
           console.log(error);
-        });
+        })
     }
 
   }]);
