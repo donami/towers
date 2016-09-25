db8dcfe1a1f1f4a6b2fb288006cb201dd49d5355
Markus Hederström <d0namiz@gmail.com>
2016-09-25 17:51:33 +0200

    Added sorting to the table on the first page

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -1,11 +1,14 @@
 angular.module('towersApp')
-  .controller('HomeController', ['$scope', 'TowerFactory', function($scope, TowerFactory) {
+  .controller('HomeController', ['$scope', 'TowerFactory', '$filter', function($scope, TowerFactory, $filter) {
 
     $scope.towers = [];
 
     $scope.totalItems = 0;
     $scope.currentPage = 1;
     $scope.numPerPage = 5;
+
+    $scope.orderBy = 'created_on';
+    $scope.reverse = false;
 
     initTowers();
 
@@ -16,9 +19,17 @@ angular.module('towersApp')
 
           $scope.towers = data;
           $scope.totalItems = response.data.length;
+          $scope.towers = $filter('orderBy')($scope.towers, $scope.orderBy, $scope.reverse);
         }, function(error) {
           $scope.status = 'Unable to load towers: ' + error.message;
         });
+    }
+
+    // Sort the table
+    $scope.sortBy = function(property) {
+      $scope.reverse = !$scope.reverse;
+      $scope.orderBy = property;
+      $scope.towers = $filter('orderBy')($scope.towers, $scope.orderBy, $scope.reverse);
     }
 
     // Filter for paginating the results
