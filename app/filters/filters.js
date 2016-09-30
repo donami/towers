7748bfd0211ab00fd11da6d4c820dba64e92c89d
Markus Hederström <d0namiz@gmail.com>
2016-10-01 00:14:55 +0200

    User can no longer select before dates for new moons that is after the 'to date'

--- app/filters/filters.js
+++ app/filters/filters.js
@@ -7,3 +7,22 @@ angular.module('towersApp').filter('validDate', ['$filter', function($filter) {
     return $filter('date')(input);
   };
 }]);
+
+// Filtering of new moons to make sure that user don't select incorrect dates
+angular.module('towersApp').filter('filterMoons', ['$filter', function($filter) {
+  return function(secondSelect, firstSelect) {
+
+    if (!firstSelect)
+      return secondSelect;
+
+    var fromDate = new Date(firstSelect.iso8601);
+
+    secondSelect = secondSelect.filter(function(obj) {
+      if (new Date(obj.iso8601) >= fromDate) {
+        return obj;
+      }
+    });
+
+    return secondSelect;
+  };
+}]);
