ff0636b7480cd4e2648a03f2fe5a929455eb5715
Markus Hederström <d0namiz@gmail.com>
2016-10-08 00:13:32 +0200

    Added filter to format to iso8601 for dates in db

--- app/filters/filters.js
+++ app/filters/filters.js
@@ -1,15 +1,16 @@
 // Filter for checking if it's a valid date, returns formated date if valid
-angular.module('towersApp').filter('validDate', ['$filter', function($filter) {
+angular.module('towersApp')
+.filter('validDate', ['$filter', function($filter) {
   return function(input) {
     if (input == '0000-00-00T00:00:00Z') {
       return '-';
     }
     return $filter('date')(input);
   };
-}]);
+}])
 
 // Filtering of new moons to make sure that user don't select incorrect dates
-angular.module('towersApp').filter('filterMoons', ['$filter', function($filter) {
+.filter('filterMoons', ['$filter', function($filter) {
   return function(secondSelect, firstSelect) {
 
     if (!firstSelect)
@@ -24,5 +25,12 @@ angular.module('towersApp').filter('filterMoons', ['$filter', function($filter) 
     });
 
     return secondSelect;
+  };
+}])
+
+.filter('dateToISO', [function() {
+  return function(input) {
+    input = new Date(input).toISOString();
+    return input;
   };
 }]);
